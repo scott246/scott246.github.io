0c9ffa33fa174a259e95577138ab35d0c9c21ff4
Nathan Scott <ndscott32@gmail.com>
2018-08-16 11:54:37 -0400

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -9,7 +9,7 @@ class About extends Component {
                 <div className="Page">
                     Hello! I'm Nathan Scott, a graduate of Purdue University with a BS in Computer Science with a concentration in Software Engineering. 
                     I graduated in May of 2017 and as of now, work as a Technology Associate for Liberty Mutual. 
-                    I live in Portsmouth, NH with my adorable cat Clover. 
+                    I live in Portsmouth, NH with my cat Clover and dog Dawson. 
                     I gained many programming skills from my coursework at Purdue, as well as my job at Liberty and my side projects, 
                     such as Design Thinking training, Java programming, iOS and Android programming, and experience with many different programming languages and systems. 
                     Thank you for visiting my website!
